6d63cb6ea0996e2c4b7ef4f220d0e8c7443f6d78
hunor64 <nhunor06@gmail.com>
2024-10-10 08:24:37 +0200

    Controller updated

--- ViccAdatbazis/Controllers/ViccController.cs
+++ ViccAdatbazis/Controllers/ViccController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ViccAdatbazis.Data;
 using ViccAdatbazis.Models;
 
@@ -8,11 +9,25 @@ namespace ViccAdatbazis.Controllers
     [ApiController]
     public class ViccController : ControllerBase
     {
+        //Adatbázis kapcsolat
         private readonly ViccDbContext _context;
-
         public ViccController(ViccDbContext context)
         {
             _context = context;
+        }
+
+        /*//Összes vicc lekérése
+        [HttpGet]
+        public ActionResult<List<Vicc>> GetViccek()
+        {
+            return _context.Viccek.Where(x => x.Aktiv == true).ToList();
+        }*/
+
+        //Összes vicc lekérése async módon
+        [HttpGet]
+        public async Task<ActionResult<List<Vicc>>> GetViccek()
+        {
+            return await _context.Viccek.Where(x => x.Aktiv == true).ToListAsync();
         }
 
         [HttpPut("{id}/like")]
