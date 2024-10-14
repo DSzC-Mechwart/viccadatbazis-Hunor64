c78853faa9af0c0872febb4364f0d71ba121e94b
hunor64 <nhunor06@gmail.com>
2024-10-14 13:49:04 +0200

    Added js script

--- ViccAdatbazis/wwwroot/js/site.js
+++ ViccAdatbazis/wwwroot/js/site.js
@@ -2,3 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function likeVicc(id) {
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("PATCH", "https://localhost:7193/api/Vicc/" + id + "/like");
+
+    xhr.send();
+}
+
+function dislikeVicc(id) {
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("PATCH", "https://localhost:7193/api/Vicc/" + id + "/dislike");
+
+    xhr.send();
+}
